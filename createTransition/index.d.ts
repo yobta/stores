a9b4d4afb8f9ac15f86b9db52bd40db9bc808b98
droganov <serge@droganov.ru>
2021-08-07 00:18:45 +0300

    fix typings

--- createTransition/index.d.ts
+++ createTransition/index.d.ts
@@ -1,9 +1,9 @@
-import { createStore, StoreState } from '../createStore/index.js'
+import { ObservableStore, StoreState } from '../createStore/index.js'
 
 export type Transition<S, A> = (state: S, ...args: A) => S
 export type TransitionTrigger<S, A> = (...args: A) => S
 
-export function createTransition<S extends createStore = StoreState<S>, A>(
+export function createTransition<S extends ObservableStore, A>(
   store: S,
   transition: Transition<StoreState<S>, A>
 ): TransitionTrigger<StoreState<S>, A>
