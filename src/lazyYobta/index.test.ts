b5f5228899c5752eb87cc2e1f512ae2f43a3557c
droganov <serge@droganov.ru>
2022-09-07 12:59:12 +0200

    improve lazy pluging test coverege

--- src/lazyYobta/index.test.ts
+++ src/lazyYobta/index.test.ts
@@ -1,18 +1,25 @@
 import { lazyYobta } from './index.js'
 
 const addMiddleware = vi.fn()
+const next = vi.fn()
 const initialState = 1
 
 it('adds middleware', () => {
-  lazyYobta({ addMiddleware, initialState })
+  lazyYobta({ addMiddleware, initialState, next })
 
   expect(addMiddleware).toHaveBeenCalledWith('idle', expect.any(Function))
 })
 
 it('resets to initial state when idle', () => {
-  lazyYobta({ addMiddleware, initialState })
+  lazyYobta({ addMiddleware, initialState, next })
 
   let idle = addMiddleware.mock.calls[0][1]
 
   expect(idle(2)).toEqual(1)
+})
+
+it("doesn't call next", () => {
+  lazyYobta({ addMiddleware, initialState, next })
+
+  expect(next).not.toHaveBeenCalled()
 })
