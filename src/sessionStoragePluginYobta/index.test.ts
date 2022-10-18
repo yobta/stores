e17dc170aebbe587b56ccc497ec254c58c3839c2
droganov <serge@droganov.ru>
2022-10-18 15:09:37 +0200

    refactore session storage plugin

--- src/sessionStoragePluginYobta/index.test.ts
+++ src/sessionStoragePluginYobta/index.test.ts
@@ -1,19 +1,113 @@
-import { replicatedYobta } from '../util/replicatedYobta/index.js'
-import { sessionStorageMiddleware } from './sessionStorageMiddleware.js'
+import {
+  YOBTA_IDLE,
+  YOBTA_NEXT,
+  YOBTA_READY,
+} from '../observableYobta/index.js'
 import { sessionStoragePluginYobta } from './index.js'
 
-vi.mock('./sessionStorageMiddleware.js', () => ({
-  sessionStorageMiddleware: vi.fn(),
-}))
-vi.mock('../util/replicatedYobta/index.js', () => ({
-  replicatedYobta: vi.fn(),
+let defaultItem = JSON.stringify('stored yobta')
+let item: string | null = defaultItem
+let getItem = vi.fn()
+let setItem = vi.fn()
+vi.stubGlobal('sessionStorage', {
+  getItem(key: string) {
+    getItem(key)
+    return item
+  },
+  setItem,
+})
+
+let encode = vi.fn()
+let decode = vi.fn()
+
+vi.mock('../util/encoderYobta/index.js', () => ({
+  encoderYobta: {
+    encode(args: any[]) {
+      encode(args)
+      return JSON.stringify(args)
+    },
+    decode(item: string) {
+      decode(item)
+      return JSON.parse(item)
+    },
+  },
 }))
 
-it('creates middleware and wraps it', () => {
-  let args = { channel: 'yobta' }
-  sessionStoragePluginYobta(args)
+const params = {
+  addMiddleware: vi.fn(),
+  next: vi.fn(),
+  initialState: 'yobta',
+}
 
-  expect(sessionStorageMiddleware).toHaveBeenCalledTimes(1)
-  expect(sessionStorageMiddleware).toHaveBeenCalledWith(args)
-  expect(replicatedYobta).toHaveBeenCalledTimes(1)
+beforeEach(() => {
+  item = defaultItem
+})
+
+it('adds middleware', () => {
+  sessionStoragePluginYobta({ channel: 'test' })(params)
+  expect(params.addMiddleware).toBeCalledTimes(3)
+  expect(params.addMiddleware).toBeCalledWith(YOBTA_READY, expect.any(Function))
+  expect(params.addMiddleware).toBeCalledWith(YOBTA_IDLE, expect.any(Function))
+  expect(params.addMiddleware).toBeCalledWith(YOBTA_NEXT, expect.any(Function))
+})
+
+it('has no side effects when created', () => {
+  sessionStoragePluginYobta({ channel: 'test' })(params)
+  expect(getItem).not.toBeCalled()
+  expect(setItem).not.toBeCalled()
+  expect(encode).not.toBeCalled()
+  expect(decode).not.toBeCalled()
+})
+
+it('recovers state from sessions storage', () => {
+  sessionStoragePluginYobta({ channel: 'test' })(params)
+  let state = params.addMiddleware.mock.calls[0][1]('ready')
+  expect(state).toEqual('stored yobta')
+
+  expect(decode).toHaveBeenCalledTimes(1)
+  expect(decode).toHaveBeenCalledWith(defaultItem)
+  expect(encode).not.toBeCalled()
+
+  expect(getItem).toBeCalledTimes(1)
+  expect(getItem).toBeCalledWith('test')
+  expect(setItem).not.toBeCalled()
+})
+
+it('defaults to initial state when no session is stored', () => {
+  item = null
+  sessionStoragePluginYobta({ channel: 'test' })(params)
+  let state = params.addMiddleware.mock.calls[0][1]('ready')
+  expect(state).toEqual('ready')
+  expect(getItem).toBeCalledWith('test')
+  expect(setItem).not.toBeCalled()
+})
+
+it('handles idle', () => {
+  sessionStoragePluginYobta({ channel: 'test' })(params)
+  let state = params.addMiddleware.mock.calls[1][1]('idle')
+
+  expect(encode).toBeCalledTimes(1)
+  expect(encode).toBeCalledWith('idle')
+  expect(decode).not.toBeCalled()
+
+  expect(setItem).toBeCalledTimes(1)
+  expect(setItem).toBeCalledWith('test', JSON.stringify('idle'))
+  expect(getItem).not.toBeCalled()
+
+  expect(state).toEqual('idle')
+})
+
+it('handles next', () => {
+  sessionStoragePluginYobta({ channel: 'test' })(params)
+  let state = params.addMiddleware.mock.calls[1][1]('next')
+
+  expect(encode).toBeCalledTimes(1)
+  expect(encode).toBeCalledWith('next')
+  expect(decode).not.toBeCalled()
+
+  expect(setItem).toBeCalledTimes(1)
+  expect(setItem).toBeCalledWith('test', JSON.stringify('next'))
+  expect(getItem).not.toBeCalled()
+
+  expect(state).toEqual('next')
 })
--- src/sessionStoragePluginYobta/index.ts
+++ src/sessionStoragePluginYobta/index.ts
@@ -1,13 +1,31 @@
-import { StorePlugin } from '../observableYobta/index.js'
-import { replicatedYobta } from '../util/replicatedYobta/index.js'
-import { BackEndFactoryProps } from '../util/BackEndYobta/index.js'
-import { sessionStorageMiddleware } from './sessionStorageMiddleware.js'
+import { encoderYobta, YobtaEncoder } from '../util/encoderYobta/index.js'
+import {
+  StorePlugin,
+  YOBTA_IDLE,
+  YOBTA_NEXT,
+  YOBTA_READY,
+} from '../observableYobta/index.js'
 
-interface BackendWrapper {
-  <State>(props: BackEndFactoryProps): StorePlugin<State>
+interface SessionStoragePluginFactory {
+  <State>(props: {
+    channel: string
+    encoder?: YobtaEncoder
+  }): StorePlugin<State>
 }
 
-export const sessionStoragePluginYobta: BackendWrapper = (...args) => {
-  let backend = sessionStorageMiddleware(...args)
-  return replicatedYobta(backend)
-}
+export const sessionStoragePluginYobta: SessionStoragePluginFactory =
+  ({ channel, encoder = encoderYobta }) =>
+  ({ addMiddleware }) => {
+    let write = (state: any) => {
+      let encodedMessage = encoder.encode(state)
+      sessionStorage.setItem(channel, encodedMessage)
+      return state
+    }
+
+    addMiddleware(YOBTA_READY, state => {
+      let item = sessionStorage.getItem(channel)
+      return item === null ? state : encoder.decode(item)
+    })
+    addMiddleware(YOBTA_IDLE, write)
+    addMiddleware(YOBTA_NEXT, write)
+  }
