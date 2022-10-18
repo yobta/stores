67441f0789d11e17f4416249d741e4f7dc966cc9
droganov <serge@droganov.ru>
2022-10-18 16:04:00 +0200

    add validation plugin

---
+++ src/validationPluginYobta/index.ts
@@ -0,0 +1,21 @@
+import { encoderYobta, YobtaEncoder } from '../util/encoderYobta/index.js'
+import {
+  StorePlugin,
+  YOBTA_IDLE,
+  YOBTA_INIT,
+  YOBTA_NEXT,
+  YOBTA_READY,
+} from '../observableYobta/index.js'
+
+interface ValidationPluginFactory {
+  <State>(validate: (input: any) => State): StorePlugin<State>
+}
+
+export const validationPluginYobta: ValidationPluginFactory =
+  validate =>
+  ({ addMiddleware }) => {
+    addMiddleware(YOBTA_INIT, validate)
+    addMiddleware(YOBTA_READY, validate)
+    addMiddleware(YOBTA_IDLE, validate)
+    addMiddleware(YOBTA_NEXT, validate)
+  }
