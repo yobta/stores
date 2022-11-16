174dff206e7335a8eea356f3d7adcb5788688fe9
droganov <serge@droganov.ru>
2022-11-16 16:24:40 +0100

    rename map store to plainObject

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './stores/mapYobta/index.js'
 export * from './stores/machineYobta/index.js'
 export * from './stores/observableYobta/index.js'
+export * from './stores/plainObjectYobta/index.js'
 export * from './stores/stackYobta/index.js'
 
 export * from './plugins/broadcastChannelPluginYobta/index.js'
