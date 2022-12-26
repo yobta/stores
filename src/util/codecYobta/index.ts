5f3c520a01a5c2d38e5f922462699bba4a02c4b4
droganov <serge@droganov.ru>
2022-12-26 13:09:28 +0100

    encoder utility dosc

--- src/util/codecYobta/index.ts
+++ src/util/codecYobta/index.ts
@@ -3,6 +3,21 @@ export interface YobtaCodec {
   decode: (item: any, fallback: () => any) => [any, ...any[]]
 }
 
+/**
+ * Object that provides encoding and decoding functionality.
+ *
+ * @namespace codecYobta
+ *
+ * @property {function} encode - Encode an item and any additional overloads as a JSON string.
+ * @param {any} item - The item to be encoded.
+ * @param {...any} overloads - Additional overloads to include in the encoded string.
+ * @returns {string} - The encoded string.
+ *
+ * @property {function} decode - Decode an item from a JSON string, falling back to a provided fallback value if necessary.
+ * @param {any} item - The item to be decoded.
+ * @param {function} fallback - A function that returns the fallback value to use if the item cannot be decoded.
+ * @returns {[any, ...any[]]} - An array containing the decoded item, followed by any additional overloads.
+ */
 export const codecYobta: YobtaCodec = {
   encode(item, ...overloads) {
     return JSON.stringify([item, ...overloads])
