dd7b146650cb4e246684e17671a7f57d7ffabf68
droganov <serge@droganov.ru>
2023-01-06 12:28:58 +0100

    update stack store typings

--- src/stores/stackYobta/index.ts
+++ src/stores/stackYobta/index.ts
@@ -8,50 +8,27 @@ import {
 interface YobtaStackFactory {
   <Item, Overloads extends any[] = any[]>(
     initialState: Set<Item> | Item[],
-    ...plugins: YobtaStorePlugin<Set<Item>, Overloads>[]
+    ...plugins: YobtaStorePlugin<ReadonlySet<Item>, Overloads>[]
   ): {
     add(member: Item, ...overloads: any[]): boolean
     last(): Item
-    observe(observer: YobtaObserver<Set<Item>, Overloads>): VoidFunction
+    observe(observer: YobtaObserver<ReadonlySet<Item>, Overloads>): VoidFunction
     on(
       event: YobtaStoreEvent,
-      handler: (state: Set<Item>) => void,
+      handler: (state: ReadonlySet<Item>) => void,
     ): VoidFunction
     remove(member: Item, ...overloads: any[]): boolean
     size(): number
   }
 }
 
-/**
- * Creates a new observable stack store.
- *
- * @template Item
- * @param {Set<Item> | Item[]} [initialState] - The initial state of the stack.
- * @param {YobtaStorePlugin<Set<Item>>[]} [plugins] - The plugins to use with the store.
- * @returns {{
- *   add: (member: Item, ...overloads: any[]) => boolean,
- *   last: () => Item,
- *   observe: (observer: YobtaObserver<Set<Item>>) => VoidFunction,
- *   remove: (member: Item, ...overloads: any[]) => boolean,
- *   size: () => number
- * }} An object with methods for interacting with the stack.
- *
- * The `add` method adds a new item to the stack, and returns `true` if the item was successfully added. If the item already exists in the stack, the method returns `false` and does not add the item.
- *
- * The `last` method returns the last item added to the stack.
- *
- * The `observe` method allows an observer function to be registered with the stack. This observer function will be called whenever the stack is updated, with the updated stack set as the first argument and any additional overloads passed as additional arguments.
- *
- * The `remove` method removes an item from the stack, and returns `true` if the item was successfully removed. If the item does not exist in the stack, the method returns `false` and does not remove the item.
- *
- * The `size` method returns the number of items in the stack.
- */
 export const stackYobta: YobtaStackFactory = <Item, Overloads extends any[]>(
   initialState?: Set<Item> | Item[],
-  ...plugins: YobtaStorePlugin<Set<Item>, Overloads>[]
+  ...plugins: YobtaStorePlugin<ReadonlySet<Item>, Overloads>[]
 ) => {
-  let { last, observe, next, on } = storeYobta<Set<Item>, Overloads>(
+  let { last, observe, next, on } = storeYobta<ReadonlySet<Item>, Overloads>(
     new Set(initialState),
+
     ...plugins,
   )
   return {
@@ -71,9 +48,9 @@ export const stackYobta: YobtaStackFactory = <Item, Overloads extends any[]>(
     observe,
     on,
     remove(item: Item, ...overloads: Overloads) {
-      let state = last()
+      let state = new Set(last())
       let result = state.delete(item)
-      if (result) next(new Set(state), ...overloads)
+      if (result) next(state, ...overloads)
       return result
     },
     size: () => last().size,
