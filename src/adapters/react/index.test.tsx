79bb27748bf1e6dfd6fb0f10cbf5ecf1eda40f04
droganov <serge@droganov.ru>
2023-01-10 12:54:26 +0100

    fix store hook spec

--- src/adapters/react/index.test.tsx
+++ src/adapters/react/index.test.tsx
@@ -1,44 +1,54 @@
-import { renderHook } from '@testing-library/react'
+import { renderHook, act } from '@testing-library/react'
 
+import { storeYobta } from '../../stores/storeYobta/index.js'
 import { useYobta } from './index.js'
 
-const last = vi.fn(() => ({ foo: 'bar' }))
-const unsubscribe = vi.fn()
-const observe = vi.fn(() => unsubscribe)
+const unsubscribeMock = vi.fn()
 
-const mockStore = {
-  last,
-  observe,
-}
+vi.mock('../../stores/storeYobta/index.js', () => ({
+  storeYobta(initialState: any) {
+    let state = initialState
+    return {
+      last: vi.fn().mockImplementation(() => state),
+      next: vi.fn().mockImplementation((nextState: any) => {
+        state = nextState
+      }),
+      observe: vi.fn().mockImplementation(() => unsubscribeMock),
+    }
+  },
+}))
+
+let store = storeYobta(1)
+
+beforeEach(() => {
+  store = storeYobta(1)
+})
 
 it('should return last state', () => {
-  let { result } = renderHook(() => useYobta(mockStore))
-  expect(result.current).toEqual({ foo: 'bar' })
+  let { result } = renderHook(() => useYobta(store))
+  expect(result.current).toEqual(1)
 })
 
 it('should subscribe to store', () => {
-  renderHook(() => useYobta(mockStore))
-  expect(observe).toHaveBeenCalled()
+  renderHook(() => useYobta(store))
+  expect(store.observe).toHaveBeenCalled()
 })
 
 it('should unsubscribe from store', () => {
-  let { unmount } = renderHook(() => useYobta(mockStore))
+  let { unmount } = renderHook(() => useYobta(store))
   unmount()
-  expect(unsubscribe).toHaveBeenCalled()
+  expect(unsubscribeMock).toHaveBeenCalled()
 })
 
 it('should handle store updates', () => {
-  let { result, rerender } = renderHook(() => useYobta(mockStore))
-  expect(last).toHaveBeenCalledTimes(2)
-  expect(result.current).toEqual({ foo: 'bar' })
-
-  // @ts-ignore
-  let observer = observe.mock.calls[0][0] as (state: any) => void
-  last.mockReturnValueOnce({ foo: 'baz' })
-  observer({ foo: 'baz' })
-  expect(last).toHaveBeenCalledTimes(2)
-
+  let { result, rerender } = renderHook(() => useYobta(store))
+  expect(store.last).toHaveBeenCalledTimes(2)
+  expect(result.current).toEqual(1)
+  act(() => {
+    store.next(2)
+  })
+  expect(store.last).toHaveBeenCalledTimes(2)
   rerender()
-  expect(last).toHaveBeenCalledTimes(3)
-  expect(result.current).toEqual({ foo: 'baz' })
+  expect(store.last).toHaveBeenCalledTimes(3)
+  expect(result.current).toEqual(2)
 })
