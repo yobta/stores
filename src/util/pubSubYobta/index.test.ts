795cf0175d51db449dec017e00c14b37a9c4e14d
droganov <serge@droganov.ru>
2023-01-26 17:15:28 +0100

    add unsubscribe  method to pubsub utility (#55)

--- src/util/pubSubYobta/index.test.ts
+++ src/util/pubSubYobta/index.test.ts
@@ -4,6 +4,7 @@ it('creates pub/sub', () => {
   let pubSub = pubSubYobta()
   expect(pubSub.publish).toBeInstanceOf(Function)
   expect(pubSub.subscribe).toBeInstanceOf(Function)
+  expect(pubSub.unsubscribe).toBeInstanceOf(Function)
 })
 
 it('notifies', () => {
@@ -19,6 +20,26 @@ it('notifies', () => {
   expect(spy).toBeCalledWith('data')
 
   unsubscribe()
+
+  pubSub.publish('topic', 'data')
+
+  expect(spy).toHaveBeenCalledOnce()
+  expect(spy).toBeCalledWith('data')
+})
+
+it('unsubscrbes', () => {
+  let spy = vi.fn()
+  let pubSub = pubSubYobta()
+  pubSub.subscribe('topic', spy)
+
+  expect(spy).not.toBeCalled()
+
+  pubSub.publish('topic', 'data')
+
+  expect(spy).toHaveBeenCalledOnce()
+  expect(spy).toBeCalledWith('data')
+
+  pubSub.unsubscribe('topic', spy)
 
   pubSub.publish('topic', 'data')
 
