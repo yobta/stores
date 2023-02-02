9ced66f5b51a419572c334482db0a38072d6e58f
droganov <serge@droganov.ru>
2023-02-02 23:44:21 +0100

    add  more test cases

--- src/stores/derivedYobta/index.test.ts
+++ src/stores/derivedYobta/index.test.ts
@@ -1,4 +1,4 @@
-import { derivedYobta } from '.'
+import { derivedYobta } from './index.js'
 import { storeYobta } from '../storeYobta/index.js'
 
 test('return type', () => {
@@ -109,6 +109,7 @@ test('diamond', () => {
   expect(derived3Observer).toBeCalledTimes(1)
 })
 
+// note: reproduceng tests from nanostores
 const replacer =
   (...args: [string, string]) =>
   (v: string) =>
@@ -186,6 +187,115 @@ test('prevents diamond dependency problem 3', () => {
 
   store.next(1)
   expect(mock.mock.calls).toEqual([['a1b1c1d1']])
+
+  unsubscribe()
+})
+
+test('prevents diamond dependency problem 4 (complex)', () => {
+  let store1 = storeYobta<number>(0)
+  let store2 = storeYobta<number>(0)
+
+  let mock1 = vi.fn()
+  let mock2 = vi.fn()
+
+  let fn =
+    (name: string) =>
+    (...v: (string | number)[]) =>
+      `${name}${v.join('')}`
+
+  let a = derivedYobta(fn('a'), store1)
+  let b = derivedYobta(fn('b'), store2)
+
+  let c = derivedYobta(fn('c'), a, b)
+  let d = derivedYobta(fn('d'), a)
+
+  let e = derivedYobta(fn('e'), c, d)
+
+  let f = derivedYobta(fn('f'), e)
+  let g = derivedYobta(fn('g'), f)
+
+  let combined1 = derivedYobta((...args) => args.join(''), e)
+  let combined2 = derivedYobta((...args) => args.join(''), e, g)
+
+  let unsubscribe1 = combined1.observe(mock1)
+  let unsubscribe2 = combined2.observe(mock2)
+
+  expect(combined1.last()).toBe('eca0b0da0')
+  expect(combined2.last()).toBe('eca0b0da0gfeca0b0da0')
+
+  store1.next(1)
+  expect(mock1.mock.calls).toEqual([['eca1b0da1']])
+  expect(mock2.mock.calls).toEqual([['eca1b0da1gfeca1b0da1']])
+
+  store2.next(2)
+  expect(mock1.mock.calls).toEqual([['eca1b0da1'], ['eca1b2da1']])
+  expect(mock2.mock.calls).toEqual([
+    ['eca1b0da1gfeca1b0da1'],
+    ['eca1b2da1gfeca1b2da1'],
+  ])
+
+  unsubscribe1()
+  unsubscribe2()
+})
+
+test('prevents diamond dependency problem 5', () => {
+  let events = ''
+  let firstName = storeYobta('John')
+  let lastName = storeYobta('Doe')
+  let fullName = derivedYobta(
+    (first, last) => {
+      events += 'full '
+      return `${first} ${last}`
+    },
+    firstName,
+    lastName,
+  )
+  let isFirstShort = derivedYobta(name => {
+    events += 'short '
+    return name.length < 10
+  }, firstName)
+  let displayName = derivedYobta(
+    (first, isShort, full) => {
+      events += 'display '
+      return isShort ? full : first
+    },
+    firstName,
+    isFirstShort,
+    fullName,
+  )
+
+  expect(events).toBe('full short display ')
+
+  displayName.observe(() => {})
+  expect(displayName.last()).toBe('John Doe')
+
+  firstName.next('Benedict')
+  expect(displayName.last()).toBe('Benedict Doe')
+  expect(events).toBe('full short display short full display ')
+
+  firstName.next('Montgomery')
+  expect(displayName.last()).toBe('Montgomery')
+  expect(events).toBe('full short display short full display short display ')
+})
+
+test('prevents diamond dependency problem 6', () => {
+  let store1 = storeYobta<number>(0)
+  let store2 = storeYobta<number>(0)
+  let mock = vi.fn()
+
+  let a = derivedYobta(v => `a${v}`, store1)
+  let b = derivedYobta(v => `b${v}`, store2)
+  let c = derivedYobta(v => v.replace('b', 'c'), b)
+
+  let combined = derivedYobta(($a, $c) => `${$a}${$c}`, a, c)
+
+  let unsubscribe = combined.observe(mock)
+
+  expect(mock).toBeCalledTimes(0)
+  expect(combined.last()).toBe('a0c0')
+
+  store1.next(1)
+  expect(mock.mock.calls).toEqual([['a1c0']])
 
   unsubscribe()
 })
